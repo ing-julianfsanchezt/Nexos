918abe9db8082732af3c854addd0b41c27cdb4ae
ing-julianfsanchezt <julianfsanchezt@gmail.com>
2021-05-22 23:57:23 -0500

    Add files via upload

---
+++ SQLQueryForNexos.sql
@@ -0,0 +1,11 @@
+
+--DATETIME FORMAT MM/DD/YYYY
+INSERT INTO NexosDB.DBO.Autor VALUES ('Gabriel Garcia Marquez', '03/06/1927', 'Aracataca', 'gabo@gmail.com');
+INSERT INTO NexosDB.DBO.Autor VALUES ('Edgar Alan Poe', '03/19/1809', 'Baltimore', 'poe@gmail.com');
+INSERT INTO NexosDB.DBO.Autor VALUES ('Jorge Luis Borges', '08/24/1899', 'Ginebra', 'jlb@gmail.com');
+
+---------------------------------------------------------------------------------------------------------
+INSERT INTO NexosDB.DBO.Editorial VALUES ('Olimpia', 'Cll 123 # 45 - 67', '555-4556', -1);
+INSERT INTO NexosDB.DBO.Editorial VALUES ('Norma', 'Cll 132 # 76 - 99', '555-9999', 5);
+INSERT INTO NexosDB.DBO.Editorial VALUES ('Planeta', 'Cll 12 # 6 - 99', '555-7777', 15);
+
